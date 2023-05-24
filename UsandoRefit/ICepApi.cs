3237bbd895ef01aa07db9140b4e60bfa1ef06934
Ramon da Silva Santos <ramon.santos@cliqx.com.br>
2023-05-24 19:42:11 -0300

    Criado tutorial Refit

---
+++ UsandoRefit/ICepApi.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoRefit
+{
+    public interface ICepApi
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> ConsultaCep(string cep);
+    }
+}
---
+++ UsandoRefit/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Refit;
+using UsandoRefit;
+
+Console.WriteLine("Digite o CEP.....");
+var cep = Console.ReadLine();
+
+var servico = RestService.For<ICepApi>("https://viacep.com.br");
+var retorno = await servico.ConsultaCep(cep);
+
+if(retorno != null)
+{
+    Console.WriteLine($"Logradouro: {retorno.Logradouro}");
+    Console.WriteLine($"Bairro: {retorno.Bairro}");
+    Console.WriteLine($"Cidade: {retorno.Localidade}");
+    Console.WriteLine($"Estado: {retorno.UF}");
+}
